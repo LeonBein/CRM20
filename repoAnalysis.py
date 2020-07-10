e851d4def0073dabc48d780e7b26e8b0fc502127
LeonBein <leonbein@web.de>
2020-07-10 16:55:47 +0200

    Create Data as non dataframe

--- repoAnalysis.py
+++ repoAnalysis.py
@@ -1,5 +1,9 @@
 import re
+import repoLibrarian
+import time
 
+stringRemoveRegex = re.compile(r"\".*?\"")
+commentRegex = re.compile(r"//.*?\n|/\*.*?\*/", re.S)
 
 def safeDivision(a,b):
     return a/b if b > 0 else 0
@@ -41,75 +45,80 @@ def timedMetricPerFileForRepo(repoTuple, metricFunction, fileCountConsumer = saf
         print('Failed to analyze '+str(repoTuple)+': '+str(e))
         return ([],[])
     
-def timedMetricPerFileForRepo(repoTuple, metricFunction, fileCountConsumer = safeDivision):
-    (user, project) = repoTuple
-    repo = getRepo(user, project)
-    timestamps = []
-    metrics = []
-    try:
-        start = time.time()
-        for commit in repo.iter_commits():
-            fileCount = 0
-            aggregatedMetric = 0
-            for obj in commit.tree.traverse():
-                if obj.type == 'blob' and obj.name.endswith('.java'):
-                    fileCount = fileCount + 1
-                    content = obj.data_stream.read().decode("CP437")#.decode("utf-8")
-                    metric = metricFunction(content)
-                    aggregatedMetric = aggregatedMetric + metric
-            metrics.append(fileCountConsumer(aggregatedMetric, fileCount))
-            timestamps.append(commit.committed_date)
+def loc(contentWithHeader, **kwargs):
+    return len(contentWithHeader.split('\n'))
 
-        end = time.time()
-        print('Time used for '+str(repoTuple)+': '+str(end - start))
-#        while len(metrics) > 0 and metrics[-1] < 1:
-#            metrics.pop()
-#            timestamps.pop()
-        return (timestamps, metrics)
-    except Exception as e:
-        print('Failed to analyze '+str(repoTuple)+': '+str(e))
-        return ([],[])
+def cloc(content, **kwargs):
+    return len(content.split('\n'))
+
+def fileCount(contentWithHeader, **kwargs):
+    return 1
 
 methodRegex = re.compile(r"(public|protected|private|static|\s) +[\w\<\>\[\]]+\s+(\w+) *\([^\)]*\) *(\{?|[^;])")
-def locmOf(contentWithHeader, **kwargs):
-    content = removeHeader(contentWithHeader)
+def numMethods(content, **kwargs):
     numMethods = occurencesOf(methodRegex, content)
-    loc = len(content.split('\n'))
-    locm = safeDivision(loc, numMethods)
-    return locm
+    return numMethods
 
 lambdaRegex = re.compile(r"->|::")
-def lambdaDensity(content, loc, **kwargs):
+def numLambdas(content, **kwargs):
     numLambdas = occurencesOf(lambdaRegex, content) 
-    return numLambdas * 1000 / loc
+    return numLambdas
     
-stringRemoveRegex = re.compile(r"\".*?\"")
-commentRegex = re.compile(r"//.*?\n|/\*.*?\*/", re.S)
-def commentDensity(content, loc, **kwargs):
-    matches = pattern.findall(stringRemoveRegex.sub("\"...\"", x))
-    # Lines of comment; trailing newlines indicate that comment ends with the newline and are therefore removed
+def numCommentLines(content, **kwargs):
+    matches = commentRegex.findall(stringRemoveRegex.sub("\"...\"", content))
+    # Lines of comment; trailing newlines indicate that comment ends with the newline and are therefore stripped
     commentLines = sum(map(lambda x: len(x.rstrip().split("\n")), matches))
-    return commentLines * 1000 / loc
+    return commentLines
 
-def reflectionDensity(content, loc, **kwargs):
-    contentWithoutStrings = stringRemoveRegex.sub("\"...\"", content)
-    contentWithoutComments = commentRegex.sub("/*...*/", contentWithoutStrings)
-    return (contentWithoutComments.count('instanceof') + contentWithoutComments.count('.class.') + contentWithoutComments.count('Class<')) * 1000 / loc
+def numReflection(contentWithoutComments, **kwargs):
+    return (contentWithoutComments.count('instanceof') + contentWithoutComments.count('.class.') + contentWithoutComments.count('Class<'))
 
 snakeRegex = re.compile(r"\w_\w")
-def snakeDensity(content, loc, **kwargs):
-    contentWithoutStrings = stringRemoveRegex.sub("\"...\"", content)
-    contentWithoutComments = commentRegex.sub("/*...*/", contentWithoutStrings)
-    return len(snakeRegex.findall(contentWithoutComments)) * 1000 / loc
+def numSnakes(contentWithoutComments, **kwargs):
+    return len(snakeRegex.findall(contentWithoutComments))
 
-def indentPerLoc(contentWithHeader, loc, **kwargs):
-    content = removeHeader(contentWithHeader)
-    indents = list(map(lambda line: len(line[:(len(line)-len(line.lstrip()))].replace('\t', '    ')) / 4, content.split('\n')))
-    avg = sum(indents)/len(indents)
-    return avg
-
-def loc(content):
-    return len(content.split('\n'))
+# Can be divided by loc to get average indent
+def totalIndent(content, **kwargs):
+    indentOfLine = lambda line: len(line[:(len(line)-len(line.lstrip()))].replace('\t', '    ')) / 4 
+    indents = list(map(indentOfLine, content.split('\n')))
+    indentSum = sum(indents)
+    return indentSum
 
 def identity(x,*y):
     return x
+
+
+metricSuite = [loc, cloc, fileCount, numMethods, numLambdas, numCommentLines, numReflection, numSnakes, totalIndent]
+    
+def calculateMetrics(repoTuple, metricSuite=metricSuite):
+    (user, project, repoId) = repoTuple
+    repo = repoLibrarian.getRepo(user, project)
+    results = []
+    try:
+        start = time.time()
+        for commit in repo.iter_commits():
+            resultTuple = {
+                'sha' : commit.hexsha,
+                'parent' : commit.parents[-1].hexsha if len(commit.parents) == 1 else None,
+                'timestamp' : commit.committed_date,
+                'repoId' : repoId
+            }
+            for metricFunction in metricSuite:
+                resultTuple[metricFunction.__name__] = 0
+            for obj in commit.tree.traverse():
+                if obj.type == 'blob' and obj.name.endswith('.java'):
+                    contentWithHeader = obj.data_stream.read().decode("CP437")#.decode("utf-8")
+                    content = removeHeader(contentWithHeader)
+                    contentWithoutStrings = stringRemoveRegex.sub("\"...\"", content)
+                    contentWithoutComments = commentRegex.sub("/*...*/", contentWithoutStrings)
+                    for metricFunction in metricSuite:
+                        metric = metricFunction(content=content, contentWithHeader=contentWithHeader, contentWithoutComments=contentWithoutComments)
+                        resultTuple[metricFunction.__name__] = resultTuple[metricFunction.__name__] + metric
+            results.append(resultTuple)
+
+        end = time.time()
+        print('Time used for '+str(repoTuple)+': '+str(end - start))
+        return results
+    except Exception as e:
+        print('Failed to analyze '+str(repoTuple)+': '+str(e))
+        return []
